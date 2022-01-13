d32130a501c1b7ae23b974cdb3525378937ad336
Janaina Falco <janainaster@gmail.com>
2022-01-13 14:57:41 -0300

    Navbar

--- frontend/src/componentes/Navbar/index.tsx
+++ frontend/src/componentes/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar(){
                 <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a target="_blank" href="https://github.com/Janainaffalco">
+                    <a href="https://github.com/Janainaffalco">
                         <div className="dsmovie-contact-container">
                         <GithubIcon />
                         <p className="dsmovie-contact-link">/devsuperior</p>
