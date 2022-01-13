81c3334957413cd446a67080ff5ed52023eb09f0
Janaina Falco <janainaster@gmail.com>
2022-01-13 15:19:43 -0300

    MovieCard

--- frontend/src/componentes/MovieCard/index.tsx
+++ frontend/src/componentes/MovieCard/index.tsx
@@ -8,7 +8,7 @@ function MovieCard() {
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AweDlIiohqw4sjC7gCsPVd1zo77.jpg",
-        title: "The Witcher",
+        title: "Mulher Maravilha 1984",
         count: 2,
         score: 4.5
     };
