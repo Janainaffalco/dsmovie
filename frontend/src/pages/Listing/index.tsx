9f444b9cb225b644de9a172fa6020636e7d9665d
Janaina Falco <janainaster@gmail.com>
2022-01-14 21:03:51 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
 import Pagination from "componentes/Pagination";
 import MovieCard from "componentes/MovieCard";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+         .then(response =>{
+
+                console.log(response.data)
+         });
 
     return (
 
